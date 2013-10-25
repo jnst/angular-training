015eb6bce33536d097f21d534ab0490cd9649151
jnst <jnst@outook.com>
2013-10-25 19:09:36 +0900

    Improve UI

--- js/tz.js
+++ js/tz.js
@@ -1,11 +1,27 @@
 function TzCtrl($scope) {
-
   $scope.sample = function() {
     return "Hello, World";
   };
-
   $scope.now = function(offset) {
     return moment().utc().add('hours', offset).format('YYYY-MM-DD HH:mm:ss');
   }
-
 }
+
+function realtime() {
+  setInterval(function() {
+    var elms = $('.realtime');
+    for (var i = 0;i < elms.length; i++) {
+      var elm = $(elms[i]);
+      var dateString = elm.html();
+      if (dateString) {
+        var next = moment(dateString, 'YYYY-MM-DD HH:mm:ss').add('seconds', 1).format('YYYY-MM-DD HH:mm:ss');
+        elm.html(next);
+      }
+    };
+  }, 1000);
+}
+
+$(function() {
+  $('[rel=popover]').popover({html: true});
+  realtime();
+});
