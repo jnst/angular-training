e5a90f9c7d35648189107a643c2394d4d4538b40
jnst <jnst@outlook.com>
2013-10-25 23:50:18 +0900

    Complete

--- js/tz.js
+++ js/tz.js
@@ -1,21 +1,46 @@
 function TzCtrl($scope) {
-  $scope.sample = function() {
+  // initial display
+  $scope.hello = function() {
     return "Hello, World";
   };
   $scope.now = function(offset) {
     return moment().utc().add('hours', offset).format('YYYY-MM-DD HH:mm:ss');
   }
+
+  // dynamic calculate for JST
+  $scope.inputJst = '';
+  $scope.resultJst = '';
+  $scope.convertToPdt = function() {
+    var hour = $scope.inputJst;
+    if (hour && (0 <= hour) && (hour <= 23)) {
+      $scope.resultJst = moment().hours(hour).add('hours', -16).format('MM/DD H時')
+    } else {
+      $scope.resultJst = '';
+    }
+  }
+  
+  // daynamic calculate for PDT
+  $scope.inputPdt = '';
+  $scope.resultPdt = '';
+  $scope.convertToJst = function() {
+    var hour = $scope.inputPdt;
+    if (hour && (0 <= hour) && (hour <= 23)) {
+      $scope.resultPdt = moment().hours(hour).add('hours', 16).format('MM/DD H時')
+    } else {
+      $scope.resultPdt = '';
+    }
+  }
 }
 
 function realtime() {
   setInterval(function() {
-    var elms = $('.realtime');
-    for (var i = 0;i < elms.length; i++) {
-      var elm = $(elms[i]);
-      var dateString = elm.html();
+    var els = $('.realtime');
+    for (var i = 0;i < els.length; i++) {
+      var el = $(els[i]);
+      var dateString = el.html();
       if (dateString) {
         var next = moment(dateString, 'YYYY-MM-DD HH:mm:ss').add('seconds', 1).format('YYYY-MM-DD HH:mm:ss');
-        elm.html(next);
+        el.html(next);
       }
     };
   }, 1000);
